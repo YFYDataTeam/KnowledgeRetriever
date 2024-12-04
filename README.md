5bca41ccbe4ad615a81ee620e1e53d6ff33e890c
chiseanchang0727 <sd2fsha627@gmail.com>
2024-12-04 18:01:11 +0800

    wip

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# KnowledgeRetriever
+# BIBot
---
+++ main.py
@@ -0,0 +1,16 @@
+import os
+import logging
+import argparse
+from src.utils import read_config
+
+def get_argument():
+    parser = argparse.ArgumentParser()
+
+
+    return parser.parse_args()
+
+
+def main():
+    pass
+if __name__ == '__main__':
+    main()
